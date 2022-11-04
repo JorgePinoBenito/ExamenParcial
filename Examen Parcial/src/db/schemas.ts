3b00cdf0ff595f3434bcfe6372fd0b92f18a883f
JorgePinoBenito <113174939+JorgePinoBenito@users.noreply.github.com>
2022-11-04 13:13:49 +0100

    Delete Examen Parcial/src directory

--- Examen Parcial/src/db/schemas.ts
+++
@@ -1,4 +0,0 @@
-import { Slots } from "../types.ts";
-import { ObjectId } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-
-export type SlotsSchema = Slots & { _id: ObjectId };
--- Examen Parcial/src/types.ts
+++
@@ -1,8 +0,0 @@
-export type Slots = {
-  day: number;
-  month: number;
-  year: number;
-  hour: number;
-  avaiable: boolean;
-  dni: string;
-};
